166fb23cb87b0061354b37da0576eeca0defd8b6
Sabrina <Smg21022@gmail.com>
2022-12-14 12:54:39 -0700

    Prints to WebPage One Option

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
-import React, { useState } from 'react';
-import Form from './Components/Form';
+import React, { useState } from "react";
+import Form from "./Components/Form";
 function App() {
-  const [members, setMembers] = useState ([]);
-  const [values, setValues] = useState ({name:'', email:'', role:''});
+  const [members, setMembers] = useState([]);
+  const [values, setValues] = useState({ name: "", email: "", role: "" });
 
   const onSubmit = () => {
     setMembers([values, ...members]);
-    setValues({name: ``, email: ``, role: ``});
-
-  }
+    setValues({ name: ``, email: ``, role: `` });
+  };
 
   const onChange = (names, value) => {
-    setValues({...value, [names]: value})
-  }
+    setValues({ ...value, [names]: value });
+  };
 
   return (
     <div className="App">
       <h1>Team Builder App!!</h1>
-      <Form
-      values={values}
-      change={onChange}
-      submit={onSubmit}
-      />
+      <Form values={values} change={onChange} submit={onSubmit} />
       {members.map((member, idx) => {
         return (
           <div key={idx}>
-            <h1>
-            Email - {member.email}
-            </h1><br />
-            <h1>
-              Name - {member.name} 
-              </h1><br />
-              <h1>
-                Role - {member.role}
-              </h1>
-            
+            <h1>Email - {member.email}</h1>
+            <br />
+            <h1>Name - {member.name}</h1>
+            <br />
+            <h1>Role - {member.role}</h1>
           </div>
-        )
+        );
       })}
     </div>
   );
